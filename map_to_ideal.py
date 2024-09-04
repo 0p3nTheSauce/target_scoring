9b18ae4ea1644e46a13628245bc5ab473c798085
Luke Goodall <ljgoodall2001@gmail.com>
2024-09-04 16:46:25 +0200

    Managed to fill in the second ring

--- map_to_ideal.py
+++ map_to_ideal.py
@@ -1,5 +1,6 @@
 import cv2 
 import numpy as np
+from collections import Counter
 
 def filter(diffs_rings):
   thresh = 1
@@ -29,7 +30,7 @@ contours_o, _ = cv2.findContours(thresh_o, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPL
 radii_i = []
 ideal_centre = ()
 score_rings_i = 0
-diffs_rings = [] #capture the differences between the radii
+diffs_rings_i = [] #capture the differences between the radii
 print("Ideal Rings")
 with open('ideal_ellipses.txt', 'r') as file:
   lines = file.readlines()
@@ -50,13 +51,13 @@ ellipses.reverse()
 radii_i.reverse()
 #Calculate the differences between the radii
 for i in range(1, len(radii_i)):
-  diffs_rings.append(radii_i[i-1] - radii_i[i])
+  diffs_rings_i.append(radii_i[i-1] - radii_i[i])
 
 for ellipse in ellipses:
   print(ellipse) 
 print("Ideal Centre: ", ideal_centre)
 print("Ideal Radii: ", radii_i)
-print("Distance between Rings: ", diffs_rings)
+print("Distance between Rings: ", diffs_rings_i)
 print("Score Rings: ", score_rings_i)
 print()
 
@@ -99,10 +100,10 @@ print("Score Rings: ", score_rings_o)
 print()
 
 ##########################################Mapping##########################################
-diffs_rings = [] #capture the differences between the radii
+diffs_rings_o = [] #capture the differences between the radii
 #Calculate diffs
 for i in range(1, len(radii_o)):
-  diffs_rings.append(radii_o[i-1] - radii_o[i])
+  diffs_rings_o.append(radii_o[i-1] - radii_o[i])
 print("Mapped Rings")
 for ellipses in mapped_ellipses:
   cv2.ellipse(mapped, ellipses, colour, thickness, cv2.LINE_8)
@@ -110,19 +111,77 @@ for ellipses in mapped_ellipses:
 print("Mapped Centre: ", ideal_centre)
 print("Mapped Radii: ", radii_o)
 # print("Distance between Rings: ", diffs_rings)
-diffs_rings = filter(diffs_rings)
+diffs_rings = filter(diffs_rings_o)
 print("Filtered Distance between Rings: ", diffs_rings)
 print("Score Rings: ", score_rings_o)
 
-# def fill_in_missing()
-
-
+def fill_in_missing(ideal_radii, ideal_diffs, original_radii, original_diffs):
+  #Fill in the missing rings
+  if len(ideal_radii) == len(original_radii):
+    return original_radii
+  mapped_radii = []
+  mapped_diffs = []
+  num_diff_r1r2 = 1 #difference between first ring and second ring
+  num_diff_r2r3 = 1 #difference between second ring and third ring
+  num_diff_r8r9r10 = 2 #difference between 8th ring and 9th ring and 9th ring and 10th ring
+  num_diff_r3r8 = 5 #difference between 3rd ring and 8th ring
+  num_diff_r10r12 = 2 #difference between 10th ring and 12th ring
+  missing_rings = [False]*12
+  #This should be one of the differences between the 3rd and 8th ring
+  most_common_diff = Counter(original_diffs).most_common(1)[0][0]
+  
+  #First handle the first two rings
+  
+  #Get last element of mapped diffs ,should be smallest
+  first_diff = original_diffs[-1]
+  smallest = min(original_diffs)
+  if first_diff == smallest: #First two rings must be correct
+    mapped_diffs.append(first_diff)
+    mapped_radii.extend([original_radii[-2], original_radii[-1]])
+  else:
+    #the smallest and second smallest ring should be smaller than the most common difference
+    smlst_av_ring = original_radii[-1] #smallest available ring
+    scnd_smlst_av_ring = original_radii[-2] #second smallest available ring
+    if scnd_smlst_av_ring < most_common_diff:
+      #We are either missing the smallest ring or the second smallest ring
       
+      #The smallest ring should be less than half the most common difference (between 3rd and 8th rings)
+      if smlst_av_ring < (most_common_diff / 2):
+        #generate the second smallest ring
+        missing_rings[-2] = True
+        smlst_ring = smlst_av_ring
+        scnd_smlst_ring = (smlst_ring / 22 ) * 42 #rough estimate
+      else:
+        #generate the smallest ring
+        missing_rings[-1] = True
+        scnd_smlst_ring = smlst_av_ring
+        smlst_ring = (scnd_smlst_ring / 42) * 22
+    else:
+      #We are missing the first two rings 
+      #generate the first two rings
+      missing_rings[-1] = True
+      missing_rings[-2] = True
+      smlst_ring = (most_common_diff / 58) * 22 #rough estimate
+      scnd_smlst_ring = (smlst_ring / 22 ) * 42
+    mapped_radii.extend([scnd_smlst_ring, smlst_ring]) 
+      
+  #quick test to see if this is working so far
+  print(missing_rings)
+  mapped_radii.extend(original_radii[:-1])
+  mapped_radii.sort()
+  return mapped_radii
+
+filled_in = np.zeros(ideal.shape, np.uint8)
+filled_radii = fill_in_missing(radii_i, diffs_rings_i, radii_o, diffs_rings_o)
+for rad in filled_radii:
+  ellipse = (ideal_centre, (rad, rad), 0)
+  cv2.ellipse(filled_in, ellipse, colour, thickness, cv2.LINE_8)
 
 ##########################################Display##########################################
 cv2.imshow("Original", thresh_o)
 cv2.imshow("Ideal", thresh_i)
 cv2.imshow("Mapped", mapped)
+cv2.imshow("Filled In", filled_in)
 
 
 # print()
