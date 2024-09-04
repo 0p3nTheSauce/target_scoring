218e54b8939198377ce1d0c50a3d6adf0e8d278b
Luke Goodall <ljgoodall2001@gmail.com>
2024-09-04 14:07:30 +0200

    Still trying to map to ideal

--- ideal_scoreboard.py
+++ ideal_scoreboard.py
@@ -28,7 +28,7 @@ def ideal(img):
 
 def ideal_centred(img, x, y):
   centre = (x // 2, y // 2)
-  color = (0, 255, 0) #Green
+  color = (255, 0, 0) #Blue
   thickness = 1
   
   radius1 = 10
@@ -44,7 +44,64 @@ def ideal_centred(img, x, y):
     radius3 += 36
     if r == 7:
       cv2.circle(img, centre, radius3+18, color, thickness, cv2.LINE_8)
-  cv2.imwrite("ideal_map.jpg", img)
+  cv2.imwrite("ideal_map_circle.jpg", img)
+ 
+def ideal_ellipses(img):
+  center = (247, 254)
+  color = (0, 255, 0)  # Green
+  thickness = 1
+  radius1 = 22
+  ellipse = (center, (radius1, radius1), 0)
+  cv2.ellipse(img, ellipse, color, thickness, cv2.LINE_8)
+  radius2 = 42
+  ellipse = (center, (radius2, radius2), 0)
+  cv2.ellipse(img, ellipse, color, thickness, cv2.LINE_8)
+  radius3 = 112
+  r = 2
+  for i in range(9):
+    ellipse = (center, (radius3, radius3), 0)
+    cv2.ellipse(img, ellipse, color, thickness, cv2.LINE_8)
+    r += 1
+    radius3 += 72
+    if r == 7:
+      ellipse = (center, (radius3+36, radius3+36), 0)
+      cv2.ellipse(img, ellipse, color, thickness, cv2.LINE_8)
+  cv2.imshow("Ideal", img)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+ 
+def ideal_centred_ellipse(img, x, y):
+  ellipses = []
+  centre = (x // 2, y // 2)
+  color = (0, 255, 0) #Blue ish green
+  thickness = 1
+  #First ring
+  radius1 = 22
+  ellipse = (centre, (radius1, radius1), 0)
+  ellipses.append(ellipse)
+  cv2.ellipse(img, ellipse, color, thickness, cv2.LINE_8)
+  #Second ring
+  radius2 = 42
+  ellipse = (centre, (radius2, radius2), 0)
+  ellipses.append(ellipse)
+  cv2.ellipse(img, ellipse, color, thickness, cv2.LINE_8)
+  #Third ring onwards
+  radius3 = 112
+  r = 2
+  for i in range(9):
+    ellipse = (centre, (radius3, radius3), 0)
+    ellipses.append(ellipse)
+    cv2.ellipse(img, ellipse, color, thickness, cv2.LINE_8)
+    r += 1
+    radius3 += 72
+    if r == 7:
+      #The 4-3 inner ring
+      ellipse = (centre, (radius3+36, radius3+36), 0)
+      ellipses.append(ellipse)
+      cv2.ellipse(img, ellipse, color, thickness, cv2.LINE_8)
+  cv2.imwrite("ideal_map_ellipse.jpg", img)
+  ellipses.sort()
+  return ellipses
   
 x, y = 700, 700
 
@@ -57,4 +114,11 @@ resized = cv2.resize(background,(500,500))
 gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 
 #ideal(gray)
-ideal_centred(image, x, y)
+#ideal_centred(image, x, y)
+
+#ideal_ellipses(gray)
+ellipses = ideal_centred_ellipse(image, x, y)
+with open("ideal_ellipses.txt", "w") as f:
+  for ellipse in ellipses:
+    f.write(str(ellipse) + "\n")
+
--- map_to_ideal.py
+++ map_to_ideal.py
@@ -2,42 +2,41 @@ import cv2
 import numpy as np
 
 original = cv2.imread("black_score.jpg", cv2.IMREAD_GRAYSCALE)
-ideal = cv2.imread("ideal_map.jpg", cv2.IMREAD_GRAYSCALE)
+ideal = cv2.imread("ideal_map_ellipse.jpg", cv2.IMREAD_GRAYSCALE)
 # resized_o = cv2.resize(original, ideal.shape)
 resized_o = cv2.resize(original, (500, 500))
-
+# resized_o = cv2.resize(original, ideal.shape)
 _, thresh_o = cv2.threshold(resized_o, 120, 255, cv2.THRESH_BINARY)
 _, thresh_i = cv2.threshold(ideal, 120, 255, cv2.THRESH_BINARY)
 
 contours_o, _ = cv2.findContours(thresh_o, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours_i, _ = cv2.findContours(thresh_i, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# contours_i, _ = cv2.findContours(thresh_i, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 ##########################################Ideal##########################################
-
-totcx_i = 0
-totcy_i = 0
-thresh = 2
-major_prev = 0
-score_rings_i = 0
+#Attributes
 radii_i = []
-
+ideal_centre = ()
+score_rings_i = 0
 
 print("Ideal Rings")
-for circle in contours_i:
-  if len(circle) < 5:
-    continue
-  ellipse = cv2.fitEllipse(circle)
+with open('ideal_ellipses.txt', 'r') as file:
+  lines = file.readlines()
+ellipses = []
+for line in lines:
+  line = line.strip()  
+  # Convert the string to a tuple and add it to the list
+  ellipse = eval(line)
   ((cx, cy), (major_axis, minor_axis), angle) = ellipse
-  if abs(major_axis - major_prev) < thresh:
-    continue
   radii_i.append(int((major_axis + minor_axis)//2))
-  major_prev = major_axis
-  print(ellipse)
-  # cv2.ellipse(mapped_ie, ellipse, (255,0,0), 1, cv2.LINE_8)
-  score_rings_i += 1
-  totcx_i += cx
-  totcy_i += cy
-ideal_centre = (int(totcx_i//score_rings_i), int(totcy_i//score_rings_i))
+  ellipses.append(ellipse)
+ideal_centre = (cx, cy)
+score_rings_i = len(ellipses)
+# Print the res
+#reverse for consistency
+ellipses.reverse()
+radii_i.reverse()
+for ellipse in ellipses:
+  print(ellipse) 
 print("Ideal Centre: ", ideal_centre)
 print("Ideal Radii: ", radii_i)
 print("Score Rings: ", score_rings_i)
@@ -51,10 +50,12 @@ totcy_o = 0
 score_rings_o = 0
 radii_o = []
 radius = 0
+thresh = 2
 #Mapped Image
 mapped = np.zeros(ideal.shape, np.uint8)
 colour = (255, 0, 0)
 thickness = 1
+mapped_ellipses = []
 
 print("Original Rings")
 for circle in contours_o:
@@ -69,7 +70,8 @@ for circle in contours_o:
   major_prev = major_axis
   print(ellipse)
   mapped_ellipse = (ideal_centre, (radius, radius), 0)
-  cv2.ellipse(mapped, mapped_ellipse, colour, thickness, cv2.LINE_8)
+  # cv2.ellipse(mapped, mapped_ellipse, colour, thickness, cv2.LINE_8)
+  mapped_ellipses.append(mapped_ellipse)
   score_rings_o += 1
   totcx_o += cx
   totcy_o += cy
@@ -80,8 +82,17 @@ print("Score Rings: ", score_rings_o)
 print()
 
 ##########################################Mapping##########################################
+print("Mapped Rings")
+for ellipses in mapped_ellipses:
+  cv2.ellipse(mapped, ellipses, colour, thickness, cv2.LINE_8)
+  print(ellipses)
+print("Mapped Centre: ", ideal_centre)
+print("Mapped Radii: ", radii_o)
+print("Score Rings: ", score_rings_o)
 
 
+
+##########################################Display##########################################
 cv2.imshow("Original", thresh_o)
 cv2.imshow("Ideal", thresh_i)
 cv2.imshow("Mapped", mapped)
