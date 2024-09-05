2ecaf14a47a1cdea8d7e7601af7af2a1b15f843b
Luke Goodall <ljgoodall2001@gmail.com>
2024-09-05 12:59:25 +0200

    fillin seems to be making some kind of a mistake on the sub ring

--- map_to_ideal.py
+++ map_to_ideal.py
@@ -341,50 +341,27 @@ print("Score Rings: ", score_rings_o)
 
 
 ##########################################Fill In##########################################
-# filled_in = np.zeros(ideal.shape, np.uint8)
-# filled_radii, filled_diffs = fill_in_missing(radii_i, diffs_rings_i, radii_o, diffs_rings_o)
+filled_in = np.zeros(ideal.shape, np.uint8)
+filled_radii, filled_diffs = fill_in_missing(radii_i, radii_o, diffs_rings_o)
+for rad in filled_radii:
+  ellipse = (ideal_centre, (rad, rad), 0)
+  cv2.ellipse(filled_in, ellipse, colour, thickness, cv2.LINE_8)
+
+# filled_radii = fill_in_missing(radii_i, radii_o, diffs_rings_o)
 # for rad in filled_radii:
 #   ellipse = (ideal_centre, (rad, rad), 0)
 #   cv2.ellipse(filled_in, ellipse, colour, thickness, cv2.LINE_8)
-filled_in = np.zeros(ideal.shape, np.uint8)
-missing = np.zeros(ideal.shape, np.uint8)
-radii_m = []
-with open('test_missing/missingr12_ellipses.txt', 'r') as file:
-  lines = file.readlines()
-ellipses_m = []
-for line in lines:
-  line = line.strip()  
-  # Convert the string to a tuple and add it to the list
-  ellipse = eval(line)
-  cv2.ellipse(missing, ellipse, colour, thickness, cv2.LINE_8)
-  ((cx, cy), (major_axis, minor_axis), angle) = ellipse
-  radius = int((major_axis + minor_axis)//2)
-  radii_m.append(radius)
-  ellipses_m.append(ellipse)
-#reverse for consistency
-ellipses_m.reverse()
-radii_m.reverse()
-#Calculate the differences between the radii
-diffs_rings_m = []
-for i in range(1, len(radii_m)):
-  diffs_rings_m.append(radii_m[i-1] - radii_m[i])
   
-filled_radii_m, filled_diffs_m = fill_in_missing(radii_i, diffs_rings_i, radii_m, diffs_rings_m)
-#filled_radii = fill_in_missing(radii_i, diffs_rings_i, radii_o, diffs_rings_o)
-for rad in filled_radii_m:
-  ellipse = (ideal_centre, (rad, rad), 0)
-  cv2.ellipse(filled_in, ellipse, colour, thickness, cv2.LINE_8)
-  
-print("Filled In Rings ", filled_radii_m)
-print("Filled In diffs ", filled_diffs_m)
+print("Filled In Rings ", filled_radii)
+print("Filled In diffs ", filled_diffs)
 
 
 ##########################################Display##########################################
 cv2.imshow("Original", thresh_o)
 cv2.imshow("Ideal", thresh_i)
 cv2.imshow("Mapped", mapped)
-cv2.imshow("Missing", missing)
 cv2.imshow("Filled In", filled_in)
+
 
 
 # print()
--- testfillin.py
+++ testfillin.py
@@ -1,12 +1,14 @@
 filled_in = np.zeros(ideal.shape, np.uint8)
+missing = np.zeros(ideal.shape, np.uint8)
 radii_m = []
-with open('missingr1_ellipses.txt', 'r') as file:
+with open('test_missing/missingr12_ellipses.txt', 'r') as file:
   lines = file.readlines()
 ellipses_m = []
 for line in lines:
   line = line.strip()  
   # Convert the string to a tuple and add it to the list
   ellipse = eval(line)
+  cv2.ellipse(missing, ellipse, colour, thickness, cv2.LINE_8)
   ((cx, cy), (major_axis, minor_axis), angle) = ellipse
   radius = int((major_axis + minor_axis)//2)
   radii_m.append(radius)
@@ -19,8 +21,7 @@ diffs_rings_m = []
 for i in range(1, len(radii_m)):
   diffs_rings_m.append(radii_m[i-1] - radii_m[i])
   
-filled_radii_m = fill_in_missing(radii_i, diffs_rings_i, radii_m, diffs_rings_m)
-#filled_radii = fill_in_missing(radii_i, diffs_rings_i, radii_o, diffs_rings_o)
-for rad in filled_radii_m:
-  ellipse = (ideal_centre, (rad, rad), 0)
-  cv2.ellipse(filled_in, ellipse, colour, thickness, cv2.LINE_8)
+filled_radii_m, filled_diffs_m = fill_in_missing(radii_i, diffs_rings_i, radii_m, diffs_rings_m)
+
+cv2.imshow("Missing", missing)
+cv2.imshow("Filled In", filled_in)
